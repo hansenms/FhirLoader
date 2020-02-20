f192f9be1d9fdd696900091ad60f565ce09e5877
Michael Hansen <mihansen@microsoft.com>
2020-02-19 20:19:00 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FhirLoader
+{
+    class Program
+    {
+        static void Main(string inputFolder, string tempFileName = "resources.json")
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
